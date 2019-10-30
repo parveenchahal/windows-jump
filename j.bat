688bd5375212b17b5a4b6611bacbdc2b318231cc
Parveen Chahal <parkuma@microsoft.com>
2019-10-30 14:07:55 +0530

    added support for navigating to different drive

--- j.bat
+++ j.bat
@@ -5,9 +5,10 @@ for /f "tokens=*" %%a in (%Jump_Map%) do (call :CheckForMatch %%a)
 exit /b
 
 :CheckForMatch
-if %p1%==%1 ( cd %2 )
+if %p1%==%1 ( for /f "tokens=1,2 delims=:" %%a in ("%2") do ( call :navigate %%a %%b ) )
 exit /b
 
 :navigate
-cd %1
+%1:
+cd %2
 exit /b
